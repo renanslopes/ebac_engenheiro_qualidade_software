86ed6f5e221982c479f409ed04848752932a5a36
Renan Lopes <renan.lop@hotmail.com>
2024-11-16 20:52:34 -0300

    Inserindo exercicio com page objects

--- Modulo_11_12/cypress/e2e/3 -loja-ebac/produtos.cy.js
+++ Modulo_11_12/cypress/e2e/3 -loja-ebac/produtos.cy.js
@@ -1,16 +1,33 @@
 /// <reference types="cypress" />
+import produtosPage from "../../support/page_objects/produtos.page";
+
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        // cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            .first()
-            .click()
+        produtosPage.buscarProdutoLista('Ajax Full-Zip Sweatshirt')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
 
     });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página de um produto', () => {
+
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+
+    });
+
+
 });
