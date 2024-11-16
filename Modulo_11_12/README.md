6b0d44a80a92adc6c2ed8548d9ee994f3305942a
Renan Lopes <renan.lop@hotmail.com>
2024-11-16 19:50:03 -0300

    Ajustando readme e local do fakerjs

---
+++ Modulo_11_12/README.md
@@ -0,0 +1,23 @@
+# Testes UI - Parte 1 e 2
+Repositório contendo exercícios sobre testes de interface utilizando Cypress (v3.16).
+Referente aos módulos 11 e 12.
+
+## Temas praticados:
+- Primeiros passos em cypress
+- Hooks
+- Massa de testes fake (Faker-JS)
+- Listas
+- Arquivo de dados (credenciais)
+- Fixtures
+- Comandos customizados (Commands)
+- Page Objects
+
+## Executando o projeto...
+```javascript
+# install dependencies
+$ npm i
+
+# Run the project
+$ npx cypress open
+```
+
---
+++ Modulo_11_12/cypress/e2e/3 -loja-ebac/login.cy.js
@@ -0,0 +1,61 @@
+/// <reference types="cypress" />
+const perfil = require('../../fixtures/perfil.json')
+
+describe('Funcionalidade: Login', () => {
+
+    beforeEach(() => {
+        cy.visit('minha-conta');
+    });
+
+    // afterEach(() => {
+    //     cy.screenshot();
+    // });
+
+    it('Deve fazer login com sucesso', () => {
+        cy.get('#username').type('renan.teste@teste.com');
+        cy.get('#password').type('123456');
+        cy.get('.woocommerce-form > .button').click();
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+            .should('contain', 'Olá, renan.teste (não é renan.teste? Sair)')
+    });
+    it('Deve exibir mensagem de erro ao inserir usuário inválido', () => {
+
+        cy.get('#username').type('renan123.teste@teste.com');
+        cy.get('#password').type('123456');
+        cy.get('.woocommerce-form > .button').click();
+        cy.get('.woocommerce-error > li')
+            .should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.');
+    });
+    it('Deve exibir mensagem de erro ao inserir senha inválida', () => {
+
+        cy.get('#username').type('renan.teste@teste.com');
+        cy.get('#password').type('12345');
+        cy.get('.woocommerce-form > .button').click();
+        cy.get('.woocommerce-error > li')
+            .should('contain', 'Erro: A senha fornecida para o e-mail renan.teste@teste.com está incorreta. Perdeu a senha?');
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario);
+        cy.get('#password').type(perfil.senha);
+        cy.get('.woocommerce-form > .button').click();
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+            .should('contain', 'Olá, renan.teste (não é renan.teste? Sair)')
+    });
+
+    it('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario, { log: false });
+            cy.get('#password').type(dados.senha, { log: false });
+            cy.get('.woocommerce-form > .button').click();
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+                .should('contain', 'Olá, renan.teste (não é renan.teste? Sair)')
+        })
+    });
+
+    it.only('Deve fazer login com sucesso - Usando comandos personalizados', () => {
+        cy.login('renan.teste@teste.com', '123456')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+            .should('contain', 'Olá, renan.teste (não é renan.teste? Sair)')
+    })
+});
