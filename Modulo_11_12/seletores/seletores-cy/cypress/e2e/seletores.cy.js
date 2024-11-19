c8521e0f042ebfafafb93ef46219dea3a5fcf9ec
Renan Lopes <renan.lop@hotmail.com>
2024-11-19 20:56:15 -0300

    Inserindo pasta de seletores

---
+++ Modulo_11_12/seletores/seletores-cy/cypress/e2e/seletores.cy.js
@@ -0,0 +1,57 @@
+///<reference types = "cypress"/>
+
+describe('Seletores avançados com cypress', () => {
+
+  beforeEach(() => {
+    cy.visit('../../seletores.html')
+  });
+
+  it('Seleciona elementos que contêm um Texto específico', () => {
+    //TODO: 
+  });
+    
+  it('Seleciona o elemento com a classe pai', () => {
+    //TODO: 
+  })
+
+  it('Seleciona o elemento com o id Filho', () => {
+    //TODO: 
+   })
+
+  it('Seleciona um elemento filho dentro do elemento com a classe pai', () => {
+    //TODO: 
+  });
+
+  it('Seleciona o segundo elemento <span> com a classe irmao', () => {
+    //TODO: 
+  });
+
+  it('Seleciona o próximo elemento irmão', () => {
+    //TODO: 
+  });
+
+  it('Seleciona o elemento irmão anterior', () => {
+    //TODO: 
+  });
+
+  it('Seleciona o irmão da div anterior', () => {
+    //TODO: 
+  });
+
+  it('Seleciona o terceiro elemento <li> encontrado', () => {
+    //TODO: 
+  });
+
+  it('Seleciona o elemento com o atributo data-test', () => {
+    //TODO: 
+  });
+
+  it('Seleciona o elemento com a classe pai do elemento com a classe filho', () => {
+    //TODO: 
+  });
+
+  it('Seleciona o elemento com um valor em um select', () => {
+    //TODO: 
+  });
+
+})
